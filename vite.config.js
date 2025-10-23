36e6b41f05cd9c1e339a04388e868cca574d11c9
Renata-Margula <reniutaaa@gmail.com>
2025-10-24 00:17:20 +0200

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/caroseryjni-portfolio/', // <- konieczne dla GitHub Pages
 })
