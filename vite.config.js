a57ee6ea84523a1b9fe8c64a82763af9926007c9
Renata-Margula <reniutaaa@gmail.com>
2025-10-24 00:37:43 +0200

    ścieżki do plików dla GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <- ścieżki będą relatywne w folderze docs
+  base: '/caroseryjni-portfolio/', // ścieżki do plików dla GitHub Pages
+  build: {
+    outDir: 'docs', // build będzie w folderze docs
+  },
 })
