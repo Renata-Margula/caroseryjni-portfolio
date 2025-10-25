69c10ed84641f856257fdf0fcabf07f4482ee6fc
Renata-Margula <reniutaaa@gmail.com>
2025-10-25 22:41:11 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/caroseryjni-portfolio',
   build: {
     outDir: 'docs', // build będzie w folderze docs
   },
